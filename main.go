be5e262854b111e1dbb1c273fd4529141e81d47a
DucSang404 <ndsang404@gmail.com>
2025-04-15 16:40:28 +0700

    prime check

--- main.go
+++ main.go
@@ -4,7 +4,12 @@ import "fmt"
 
 
 func IsPrime(n int) bool {
-	panic("not implemented")
+	for i := 2; i*i <= n; i++ {
+		if n%i == 0 {
+			return false
+		}
+	}
+	return n > 1
 }
 func main() {
 	fmt.Println("Hello, World!")
